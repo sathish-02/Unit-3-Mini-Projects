6cc98a84fc0abe4aa732653887f3ce949fea558c
sathish <sathishb02021996@gmail.com>
2022-01-11 21:42:29 +0530

    Day-12

---
+++ Day-12/navbar/navbar.js
@@ -0,0 +1,19 @@
+function navbar(){
+    return `<div id="nav">
+    <div>
+        <h1>Order Food</h1>
+    </div>
+    <div>
+       <input type="text">
+       <button>search</button>
+    </div>
+    <div>
+        <a href="login.html"><h2 id="login">Login</h2></a>
+        <a href="signup.html"><h2 id="Count">Signup</h2></a>
+        <a href="index.html"><h2 id="Count">Home</h2></a>
+        <a href="cart.html"><h2 id="Count">Cart</h2></a>
+    </div>
+</div>`
+}
+
+export default navbar;
